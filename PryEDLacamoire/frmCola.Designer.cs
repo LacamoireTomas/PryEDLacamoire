98c80d59d1d569a2029319fa9b29f99808e43ff0
root <mgiraudo@ies21.edu.ar>
2023-08-28 10:50:29 -0300

    Agregar archivos de proyecto.

---
+++ PryEDLacamoire/frmCola.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace PryEDLacamoire
+{
+    partial class frmCola
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picboxRepGraficaCola = new System.Windows.Forms.PictureBox();
+            this.mrcNuevoElemento = new System.Windows.Forms.GroupBox();
+            this.mrcElementoEliminado = new System.Windows.Forms.GroupBox();
+            this.mrcCola = new System.Windows.Forms.GroupBox();
+            this.lstCola = new System.Windows.Forms.ListBox();
+            this.dgvCola = new System.Windows.Forms.DataGridView();
+            this.lblCodigoNuevo = new System.Windows.Forms.Label();
+            this.lblNombreNuevo = new System.Windows.Forms.Label();
+            this.lblTramiteNuevo = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblTramiteEliminado = new System.Windows.Forms.Label();
+            this.lblNombreEliminado = new System.Windows.Forms.Label();
+            this.lblCodigoEliminado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxRepGraficaCola)).BeginInit();
+            this.mrcNuevoElemento.SuspendLayout();
+            this.mrcElementoEliminado.SuspendLayout();
+            this.mrcCola.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCola)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picboxRepGraficaCola
+            // 
+            this.picboxRepGraficaCola.BackgroundImage = global::PryEDLacamoire.Properties.Resources.RepGraficaCola;
+            this.picboxRepGraficaCola.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picboxRepGraficaCola.Location = new System.Drawing.Point(2, -3);
+            this.picboxRepGraficaCola.Name = "picboxRepGraficaCola";
+            this.picboxRepGraficaCola.Size = new System.Drawing.Size(195, 230);
+            this.picboxRepGraficaCola.TabIndex = 0;
+            this.picboxRepGraficaCola.TabStop = false;
+            // 
+            // mrcNuevoElemento
+            // 
+            this.mrcNuevoElemento.Controls.Add(this.btnAgregar);
+            this.mrcNuevoElemento.Controls.Add(this.lblTramiteNuevo);
+            this.mrcNuevoElemento.Controls.Add(this.lblNombreNuevo);
+            this.mrcNuevoElemento.Controls.Add(this.lblCodigoNuevo);
+            this.mrcNuevoElemento.Location = new System.Drawing.Point(203, 8);
+            this.mrcNuevoElemento.Name = "mrcNuevoElemento";
+            this.mrcNuevoElemento.Size = new System.Drawing.Size(229, 219);
+            this.mrcNuevoElemento.TabIndex = 1;
+            this.mrcNuevoElemento.TabStop = false;
+            this.mrcNuevoElemento.Text = "Nuevo Elemento";
+            // 
+            // mrcElementoEliminado
+            // 
+            this.mrcElementoEliminado.Controls.Add(this.lblCodigoEliminado);
+            this.mrcElementoEliminado.Controls.Add(this.lblNombreEliminado);
+            this.mrcElementoEliminado.Controls.Add(this.lblTramiteEliminado);
+            this.mrcElementoEliminado.Controls.Add(this.btnEliminar);
+            this.mrcElementoEliminado.Location = new System.Drawing.Point(438, 8);
+            this.mrcElementoEliminado.Name = "mrcElementoEliminado";
+            this.mrcElementoEliminado.Size = new System.Drawing.Size(229, 219);
+            this.mrcElementoEliminado.TabIndex = 2;
+            this.mrcElementoEliminado.TabStop = false;
+            this.mrcElementoEliminado.Text = "Elemento Eliminado";
+            // 
+            // mrcCola
+            // 
+            this.mrcCola.Controls.Add(this.dgvCola);
+            this.mrcCola.Controls.Add(this.lstCola);
+            this.mrcCola.Location = new System.Drawing.Point(2, 233);
+            this.mrcCola.Name = "mrcCola";
+            this.mrcCola.Size = new System.Drawing.Size(665, 270);
+            this.mrcCola.TabIndex = 3;
+            this.mrcCola.TabStop = false;
+            this.mrcCola.Text = "Listado en una Lista y una Grilla";
+            // 
+            // lstCola
+            // 
+            this.lstCola.FormattingEnabled = true;
+            this.lstCola.Location = new System.Drawing.Point(11, 21);
+            this.lstCola.Name = "lstCola";
+            this.lstCola.Size = new System.Drawing.Size(194, 238);
+            this.lstCola.TabIndex = 0;
+            // 
+            // dgvCola
+            // 
+            this.dgvCola.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCola.Location = new System.Drawing.Point(227, 19);
+            this.dgvCola.Name = "dgvCola";
+            this.dgvCola.Size = new System.Drawing.Size(432, 240);
+            this.dgvCola.TabIndex = 1;
+            // 
+            // lblCodigoNuevo
+            // 
+            this.lblCodigoNuevo.AutoSize = true;
+            this.lblCodigoNuevo.Location = new System.Drawing.Point(6, 39);
+            this.lblCodigoNuevo.Name = "lblCodigoNuevo";
+            this.lblCodigoNuevo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigoNuevo.TabIndex = 0;
+            this.lblCodigoNuevo.Text = "Código:";
+            // 
+            // lblNombreNuevo
+            // 
+            this.lblNombreNuevo.AutoSize = true;
+            this.lblNombreNuevo.Location = new System.Drawing.Point(6, 75);
+            this.lblNombreNuevo.Name = "lblNombreNuevo";
+            this.lblNombreNuevo.Size = new System.Drawing.Size(47, 13);
+            this.lblNombreNuevo.TabIndex = 1;
+            this.lblNombreNuevo.Text = "Nombre:";
+            // 
+            // lblTramiteNuevo
+            // 
+            this.lblTramiteNuevo.AutoSize = true;
+            this.lblTramiteNuevo.Location = new System.Drawing.Point(6, 108);
+            this.lblTramiteNuevo.Name = "lblTramiteNuevo";
+            this.lblTramiteNuevo.Size = new System.Drawing.Size(45, 13);
+            this.lblTramiteNuevo.TabIndex = 2;
+            this.lblTramiteNuevo.Text = "Trámite:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(26, 143);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(178, 55);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(26, 143);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(178, 55);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // lblTramiteEliminado
+            // 
+            this.lblTramiteEliminado.AutoSize = true;
+            this.lblTramiteEliminado.Location = new System.Drawing.Point(6, 108);
+            this.lblTramiteEliminado.Name = "lblTramiteEliminado";
+            this.lblTramiteEliminado.Size = new System.Drawing.Size(45, 13);
+            this.lblTramiteEliminado.TabIndex = 5;
+            this.lblTramiteEliminado.Text = "Trámite:";
+            // 
+            // lblNombreEliminado
+            // 
+            this.lblNombreEliminado.AutoSize = true;
+            this.lblNombreEliminado.Location = new System.Drawing.Point(6, 75);
+            this.lblNombreEliminado.Name = "lblNombreEliminado";
+            this.lblNombreEliminado.Size = new System.Drawing.Size(47, 13);
+            this.lblNombreEliminado.TabIndex = 6;
+            this.lblNombreEliminado.Text = "Nombre:";
+            // 
+            // lblCodigoEliminado
+            // 
+            this.lblCodigoEliminado.AutoSize = true;
+            this.lblCodigoEliminado.Location = new System.Drawing.Point(6, 39);
+            this.lblCodigoEliminado.Name = "lblCodigoEliminado";
+            this.lblCodigoEliminado.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigoEliminado.TabIndex = 7;
+            this.lblCodigoEliminado.Text = "Código:";
+            // 
+            // frmCola
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 515);
+            this.Controls.Add(this.mrcCola);
+            this.Controls.Add(this.mrcElementoEliminado);
+            this.Controls.Add(this.mrcNuevoElemento);
+            this.Controls.Add(this.picboxRepGraficaCola);
+            this.Name = "frmCola";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Estructura Dinámica no Lineal - Cola";
+            ((System.ComponentModel.ISupportInitialize)(this.picboxRepGraficaCola)).EndInit();
+            this.mrcNuevoElemento.ResumeLayout(false);
+            this.mrcNuevoElemento.PerformLayout();
+            this.mrcElementoEliminado.ResumeLayout(false);
+            this.mrcElementoEliminado.PerformLayout();
+            this.mrcCola.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCola)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picboxRepGraficaCola;
+        private System.Windows.Forms.GroupBox mrcNuevoElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblTramiteNuevo;
+        private System.Windows.Forms.Label lblNombreNuevo;
+        private System.Windows.Forms.Label lblCodigoNuevo;
+        private System.Windows.Forms.GroupBox mrcElementoEliminado;
+        private System.Windows.Forms.Label lblCodigoEliminado;
+        private System.Windows.Forms.Label lblNombreEliminado;
+        private System.Windows.Forms.Label lblTramiteEliminado;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.GroupBox mrcCola;
+        private System.Windows.Forms.DataGridView dgvCola;
+        private System.Windows.Forms.ListBox lstCola;
+    }
+}
---
+++ PryEDLacamoire/frmDatosDesarrollador.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PryEDLacamoire
+{
+    partial class frmDatosDesarrollador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDNI = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblMateria = new System.Windows.Forms.Label();
+            this.picboxFoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDNI
+            // 
+            this.lblDNI.AutoSize = true;
+            this.lblDNI.Location = new System.Drawing.Point(214, 49);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(80, 13);
+            this.lblDNI.TabIndex = 2;
+            this.lblDNI.Text = "DNI: 43559514";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(214, 90);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(134, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre: Tomas Lacamoire";
+            // 
+            // lblMateria
+            // 
+            this.lblMateria.AutoSize = true;
+            this.lblMateria.Location = new System.Drawing.Point(214, 128);
+            this.lblMateria.Name = "lblMateria";
+            this.lblMateria.Size = new System.Drawing.Size(147, 13);
+            this.lblMateria.TabIndex = 4;
+            this.lblMateria.Text = "Materia: Estructuras de Datos";
+            // 
+            // picboxFoto
+            // 
+            this.picboxFoto.BackgroundImage = global::PryEDLacamoire.Properties.Resources.foto;
+            this.picboxFoto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picboxFoto.Location = new System.Drawing.Point(12, 21);
+            this.picboxFoto.Name = "picboxFoto";
+            this.picboxFoto.Size = new System.Drawing.Size(161, 185);
+            this.picboxFoto.TabIndex = 0;
+            this.picboxFoto.TabStop = false;
+            // 
+            // frmDatosDesarrollador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 485);
+            this.Controls.Add(this.lblMateria);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDNI);
+            this.Controls.Add(this.picboxFoto);
+            this.Name = "frmDatosDesarrollador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DatosDesarrollador";
+            ((System.ComponentModel.ISupportInitialize)(this.picboxFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picboxFoto;
+        private System.Windows.Forms.Label lblDNI;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblMateria;
+    }
+}
