84a71e8ba072873ccf4e6f02c47ab162bf1835e0
root <mgiraudo@ies21.edu.ar>
2023-10-05 11:44:19 -0300

    se

---
+++ PryEDLacamoire/clsBaseDatos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace PryEDLacamoire
+{
+    internal class clsBaseDatos
+    {
+        private OleDbConnection conexion = new  OleDbConnection();
+        private OleDbCommand comando = new OleDbCommand();
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+
+        private string CadenaConexion = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Libreria.mdb";
+        private string varCadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Libreria.mdb";
+        
+
+        public void Listar (DataGridView Grilla)
+        {
+            try
+            {
+                conexion.ConnectionString = varCadenaConexion;
+                conexion.Open();
+
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.Text;
+                comando.CommandText = "SELECT* FROM LIBRO";
+
+                //tabla en la ram
+                DataSet DS = new DataSet();
+                adaptador = new OleDbDataAdapter(comando);
+                adaptador.Fill(DS, "Resultado");
+
+                Grilla.DataSource = null;
+                Grilla.DataSource = DS.Tables["Resultado"];
+
+
+
+                conexion.Close();
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+                conexion.Close();
+               
+            }
+        }
+
+
+    }
+}
