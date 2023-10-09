bb1265dca4254a102170fa735242a153d9368508
LacamoireTomas <tomaslacamoire@gmail.com>
2023-10-09 07:02:00 -0300

    s

---
+++ PryEDLacamoire/frmConsultaBaseDatos.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PryEDLacamoire
+{
+    partial class frmConsultaBaseDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblConsultaSql = new System.Windows.Forms.Label();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblConsultaSql
+            // 
+            this.lblConsultaSql.AutoSize = true;
+            this.lblConsultaSql.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConsultaSql.Location = new System.Drawing.Point(13, 13);
+            this.lblConsultaSql.Name = "lblConsultaSql";
+            this.lblConsultaSql.Size = new System.Drawing.Size(150, 20);
+            this.lblConsultaSql.TabIndex = 0;
+            this.lblConsultaSql.Text = "Consulta en SQL:";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(586, 222);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 1;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // frmConsultaBaseDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(676, 515);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.lblConsultaSql);
+            this.Name = "frmConsultaBaseDatos";
+            this.Text = "frmConsultaBaseDatos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblConsultaSql;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
---
+++ PryEDLacamoire/frmRepasoOperaciones.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PryEDLacamoire
+{
+    partial class frmRepasoOperaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOperacion = new System.Windows.Forms.Label();
+            this.cmbOperacion = new System.Windows.Forms.ComboBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperacion
+            // 
+            this.lblOperacion.AutoSize = true;
+            this.lblOperacion.Location = new System.Drawing.Point(12, 9);
+            this.lblOperacion.Name = "lblOperacion";
+            this.lblOperacion.Size = new System.Drawing.Size(200, 13);
+            this.lblOperacion.TabIndex = 0;
+            this.lblOperacion.Text = "Operación a realizar en la base de datos:";
+            // 
+            // cmbOperacion
+            // 
+            this.cmbOperacion.FormattingEnabled = true;
+            this.cmbOperacion.Location = new System.Drawing.Point(218, 6);
+            this.cmbOperacion.Name = "cmbOperacion";
+            this.cmbOperacion.Size = new System.Drawing.Size(362, 21);
+            this.cmbOperacion.TabIndex = 1;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(586, 4);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 2;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // frmRepasoOperaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 515);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.cmbOperacion);
+            this.Controls.Add(this.lblOperacion);
+            this.Name = "frmRepasoOperaciones";
+            this.Text = "frmRepasoOperaciones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOperacion;
+        private System.Windows.Forms.ComboBox cmbOperacion;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
